d6ed89775eff1f24bcc613b89608d8b6853acb35
Francisco Orlandini <francisco.orlandini@gmail.com>
2018-02-05 23:15:59 -0200

    [ongoing] trying to compile some classes extracted from deal.II

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
+//#include "exceptions.h" //ok!
+//#include "thread_local_storage.h" //ok!
+//#include "subscriptor.h"
+//#include "parameter_handler.h"
 #include <iostream>
 
 int main(int argc, char *argv[]) {    
+//		Subscriptor test;
     std::cout<<"Hello world"<<std::endl;
 
     return 0;
